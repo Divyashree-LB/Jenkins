e0c5cbaf6eb8d8427aa01ef3306e44c27408f6b6
Ubuntu <ubuntu@ip-172-31-1-191.ap-south-1.compute.internal>
2025-11-12 03:19:29 +0000

    my-first-pipeline

---
+++ pipeline_test.py
@@ -0,0 +1,4 @@
+import datetime
+name = 'Divya'
+print(f'Hi {name}! This is your first Jenkins pipeline run on {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}')
+print('Succesfully deployed')
